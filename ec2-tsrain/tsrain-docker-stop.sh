f0c9e76a6bd51707ca8c3f1de48afdfec78bdec0
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-08-21 18:33:13 +0900

    Add files via upload

--- ec2-tsrain/tsrain-docker-stop.sh
+++ ec2-tsrain/tsrain-docker-stop.sh
@@ -4,4 +4,4 @@ CONTAINER_NAME=tsrain
 CONTAINER_ID=`docker container ls -q -f "name=${CONTAINER_NAME}"`
 if [ ! -z "${CONTAINER_ID}" ]; then
   docker container stop ${CONTAINER_ID} > /dev/null 2>&1
-done
+fi
