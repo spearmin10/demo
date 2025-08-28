42ea43c22ce23ae570e8eaf9e1d0cd684f885bec
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-08-28 14:29:26 +0900

    Add files via upload

---
+++ tsrain-installer/ec2-al2023/tsrain-start.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+CONTAINER_NAME=tsrain
+TSRAIN_HOME=/opt/tsrain
+TSRAIN_PKI_PATH=${TSRAIN_HOME}/pki
+TSRAIN_CREDS_PATH="/var/opt/tsrain/services/${CONTAINER_NAME}/credentials.json"
+TSRAIN_MAILBOX_PASSWORD=TsrainDefault0!
+RAINLOOP_DEFAULT_ADMIN_PASSWORD_PATH="/var/opt/tsrain/services/${CONTAINER_NAME}/rainloop-default-admin-password.txt"
+
+cd `dirname $0`
+
+LATEST_TAG=`curl -s https://registry.hub.docker.com/v2/repositories/spearmint/tsrain/tags | jq -r '.results[].name' | sort -r --version-sort | head -1`
+
+if [ -z "${LATEST_TAG}" ]; then
+  TSRAIN_IMAGE=`docker images --format "{{.Repository}}:{{.Tag}}" | grep -E "^spearmint/tsrain:" | sort -r | head -1`
+else
+  TSRAIN_IMAGE=spearmint/tsrain:${LATEST_TAG}
+fi
+
+TSRAIN_CONTAINER_ID=`docker container ls -q -f "name=${CONTAINER_NAME}"`
+if [ -z "${TSRAIN_CONTAINER_ID}" ]; then
+  mkdir -p `dirname "${TSRAIN_CREDS_PATH}"`
+
+  if [ ! -f ${TSRAIN_CREDS_PATH} ]; then
+    jq --arg password "${TSRAIN_MAILBOX_PASSWORD}" -n '."*".password = $password' > ${TSRAIN_CREDS_PATH}
+  fi
+  chmod 600 ${TSRAIN_CREDS_PATH}
+
+  RAINLOOP_DEFAULT_ADMIN_PASSWORD=`openssl rand -base64 24`
+  echo ${RAINLOOP_DEFAULT_ADMIN_PASSWORD} > ${RAINLOOP_DEFAULT_ADMIN_PASSWORD_PATH}
+  chmod 600 ${RAINLOOP_DEFAULT_ADMIN_PASSWORD_PATH}
+
+  export RAINLOOP_DEFAULT_ADMIN_PASSWORD
+  docker container run --rm --memory 384m --memory-swap 2g \
+    -p 25:25 -p 80:88 -p 143:143 -p 443:443 -p 465:465 -p 993:993 \
+    --name "${CONTAINER_NAME}" \
+    -e RAINLOOP_DEFAULT_ADMIN_PASSWORD \
+    --mount "type=bind,source=${TSRAIN_PKI_PATH},target=/usr/local/etc/pki" \
+    --mount "type=bind,source=${TSRAIN_CREDS_PATH},target=/var/opt/testserv/credentials.json" \
+    "${TSRAIN_IMAGE}"
+else
+  echo "container ${CONTAINER_NAME} is already active"
+  exit 1
+fi
