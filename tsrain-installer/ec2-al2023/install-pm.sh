b12b4b54faddeca8f600b663ae9cb67b71f0a962
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2025-09-14 17:43:57 +0900

    Update install-pm.sh

--- tsrain-installer/ec2-al2023/install-pm.sh
+++ tsrain-installer/ec2-al2023/install-pm.sh
@@ -5,51 +5,33 @@ TSRAIN_PKI_DIR=${TSRAIN_HOME}/pki
 TSRAIN_BIN_DIR=${TSRAIN_HOME}/bin
 TSRAIN_ETC_DIR=${TSRAIN_HOME}/etc
 
-if [ "$(id -u)" -ne 0 ]; then
-  echo "The script must be run as root"
+error_exit() {
+  echo "Installation Failed." 1>&2
   exit 1
-fi
+}
 
-if [ -z "$(systemctl status tsrain 2> /dev/null)" ]; then
-  echo "TSRAIN must be installed before tsrain-pm."
-fi
+install_system_packages() {
+  dnf install -y go || error_exit
+}
 
-### Setup System
-dnf install -y go
+download_files() {
+  cd ${TSRAIN_BIN_DIR}
+  for file in tsrain-pm.go tsrain-pm.json tsrain-pm-start.sh tsrain-pm-stop.sh
+  do
+    rm -f ${file}
+    curl -s -L -J -O https://github.com/spearmin10/demo/blob/main/tsrain-installer/ec2-al2023/${file}?raw=true
+    if [ $? -ne 0 ]; then
+      echo "Failed to download ${file}."
+      exit 1
+    fi
+    if [[ "$file" == *.sh ]]; then
+      chmod +x ${file}
+    fi
+  done
+}
 
-### Setup TSRAIN Protocol Multiplexer
-mkdir -p ${TSRAIN_BIN_DIR}
-cd ${TSRAIN_BIN_DIR}
-for file in tsrain-pm.go tsrain-pm.json tsrain-pm-start.sh tsrain-pm-stop.sh
-do
-  rm -f ${file}
-  curl -s -L -J -O https://github.com/spearmin10/demo/blob/main/tsrain-installer/ec2-al2023/${file}?raw=true
-  if [ $? -ne 0 ]; then
-    echo "Failed to download ${file}."
-    exit 1
-  fi
-  if [[ "$file" == *.sh ]]; then
-    chmod +x ${file}
-  fi
-done
-
-cat << __EOT__ > /etc/systemd/system/tsrain-pm.service
-[Unit]
-Description = TSRAIN Protocol Multiplexer
-After = tsrain.service
-
-[Service]
-ExecStart = ${TSRAIN_BIN_DIR}/tsrain-pm-start.sh
-ExecStop = ${TSRAIN_BIN_DIR}/tsrain-pm-stop.sh
-Restart = always
-Type = simple
-
-[Install]
-WantedBy = multi-user.target
-__EOT__
-
-mkdir -p ${TSRAIN_ETC_DIR}
-cat << __EOT__ > ${TSRAIN_ETC_DIR}/tsrain-pm.json
+install_tsrain_pm() {
+  cat << __EOT__ > ${TSRAIN_ETC_DIR}/tsrain-pm.json || error_exit
 {
   "services": {
     "smtp_port": 25,
@@ -67,24 +49,67 @@ cat << __EOT__ > ${TSRAIN_ETC_DIR}/tsrain-pm.json
 }
 __EOT__
 
+  cd ${TSRAIN_PKI_DIR}
 
-### Issue client certificates
-for name in smtp imap4 rainloop
-do
-  openssl req \
-   -newkey ec:<(openssl ecparam -name prime256v1) \
-   -nodes \
-   -subj "/C=JP/O=Spearmint/CN=${name}-client" \
-   -keyout ${TSRAIN_PKI_DIR}/tsrain-${name}-client.key.pem | \
-    openssl x509 -req \
-     -CA ${TSRAIN_PKI_DIR}/tsrain-root.cer.pem \
-     -CAkey ${TSRAIN_PKI_DIR}/tsrain-root.key.pem \
-     -set_serial 0x$(openssl rand -hex 16) \
-     -days 365 \
-     -extensions EXTS -extfile <(printf "[EXTS]\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=clientAuth\nbasicConstraints=CA:FALSE") \
-     -out ${TSRAIN_PKI_DIR}/tsrain-${name}-client.cer.pem
-  chmod 600 ${TSRAIN_PKI_DIR}/tsrain-${name}-client.key.pem
-done
+  for name in smtp imap4 rainloop
+  do
+    openssl req \
+     -newkey ec:<(openssl ecparam -name prime256v1) \
+     -nodes \
+     -subj "/C=JP/O=Spearmint/CN=${name}-client" \
+     -keyout tsrain-${name}-client.key.pem | \
+      openssl x509 -req \
+       -CA tsrain-root.cer.pem \
+       -CAkey tsrain-root.key.pem \
+       -set_serial 0x$(openssl rand -hex 16) \
+       -days 365 \
+       -extensions EXTS -extfile <(printf "[EXTS]\nkeyUsage=digitalSignature,keyEncipherment\nextendedKeyUsage=clientAuth\nbasicConstraints=CA:FALSE") \
+       -out tsrain-${name}-client.cer.pem || error_exit
+    chmod 600 tsrain-${name}-client.key.pem || error_exit
+  done
+}
+
+install_tsrain_pm_service() {
+  cat << __EOT__ > /etc/systemd/system/tsrain-pm.service || error_exit
+[Unit]
+Description = TSRAIN Protocol Multiplexer
+After = tsrain.service
+
+[Service]
+ExecStart = ${TSRAIN_BIN_DIR}/tsrain-pm-start.sh
+ExecStop = ${TSRAIN_BIN_DIR}/tsrain-pm-stop.sh
+Restart = always
+Type = simple
+
+[Install]
+WantedBy = multi-user.target
+__EOT__
+
+  systemctl enable tsrain-pm
+}
+
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "The script must be run as root"
+  exit 1
+fi
+
+if [ -z "$(systemctl status tsrain 2> /dev/null)" ]; then
+  echo "TSRAIN must be installed before tsrain-pm."
+fi
+
+mkdir -p ${TSRAIN_BIN_DIR} || error_exit
+mkdir -p ${TSRAIN_PKI_DIR} || error_exit
+mkdir -p ${TSRAIN_ETC_DIR} || error_exit
+
+### Setup System
+install_system_packages
+
+### Setup TSRAIN Protocol Multiplexer
+download_files
+issue_certificates
+install_tsrain_pm
+install_tsrain_pm_service
 
 # Start the service
 systemctl enable tsrain-pm
